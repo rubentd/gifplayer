c9aa6fb7329f1a09450590a99d197bbe0c506f18
Yapo:Rubn Torres <ruben@schibsted.cl>
2014-02-01 17:32:26 -0300

    Removed unused css class

--- js/jquery.gifplayer.js
+++ js/jquery.gifplayer.js
@@ -1,5 +1,5 @@
 /* 
- * Gifplayer v0.1.1
+ * Gifplayer v0.1.3
  * Customizable jquery plugin to play and stop animated gifs. Similar to 9gag's
  * (c)2014 Rubén Torres - rubentdlh@gmail.com
  * Released under the MIT license
