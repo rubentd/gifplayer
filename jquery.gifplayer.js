d3534f9a233f7f2e25c4b7d0723b0cc9d95a4986
Ruben Torres <rubentdlh@gmail.com>
2015-03-19 14:14:45 -0300

    Update version to 0.2.2

--- jquery.gifplayer.js
+++ jquery.gifplayer.js
@@ -1,5 +1,5 @@
 /* 
-* Gifplayer v0.2.1
+* Gifplayer v0.2.2
 * Customizable jquery plugin to play and stop animated gifs. Similar to 9gag's
 * (c)2014 Rubén Torres - rubentdlh@gmail.com
 * Released under the MIT license
